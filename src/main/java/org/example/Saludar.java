bd4a56485c6acc38575360d8ea3da7dbf3d2bc91
= <anto041206@gmail.com>
2025-03-12 12:05:58 +0100

    commit3 antonio-JavaDoc Saludar

--- src/main/java/org/example/Saludar.java
+++ src/main/java/org/example/Saludar.java
@@ -1,6 +1,13 @@
 package org.example;
-
+/**
+ * @author antonio8 aaron
+ * @version version corregida
+ */
 public class Saludar {
+    /**
+     *
+     * @param persona recibe persona para obtener su nombre y saludar
+     */
     static void imprimirSaludo(Persona persona) {
         if (persona.getEdad() < 18) {
             System.out.println("Hola, joven " + persona.getNombre());
