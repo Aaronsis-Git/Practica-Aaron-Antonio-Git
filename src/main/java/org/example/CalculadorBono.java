897d110970becc5f7228dfaa3d99b09dbeb0df74
= <anto041206@gmail.com>
2025-03-12 11:58:53 +0100

    commit1 antonio-JavaDoc CalcularBono

--- src/main/java/org/example/CalculadorBono.java
+++ src/main/java/org/example/CalculadorBono.java
@@ -1,6 +1,13 @@
 package org.example;
 
+/**
+ * @author antonio8 aaron
+ * @version version corregida
+ */
 public class CalculadorBono {
+    /**
+     * @param persona recipe persona de tipo persona para calcular su bono
+     */
     static void calcularBono(Persona persona) {
         double bono = 0;
         if (persona.getEdad() > 50) {
